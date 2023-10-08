7786c65923155000e906467ecb9065bb9f25db89
Neha Vishwakarma <nehavishwakarma7777@gmail.com>
2023-10-08 05:57:08 +0530

    training

--- src/DeepClassifier/entity/__init__.py
+++ src/DeepClassifier/entity/__init__.py
@@ -1,1 +1,1 @@
-from DeepClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from DeepClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
